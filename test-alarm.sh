b4246bdb4def1bb8da896db31309a4c8147ca545
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-11 20:41:20 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-echo "$(<$1.test)"
 ./a.out < $1.test > $1.out 2>&1
 
 for i in $n_procs; do
@@ -12,9 +11,9 @@ done
 for pid in ${pids[*]}; do
     wait $pid
 done
-echo "Running Test Case $1 /n/n"
-echo "Looking For Expected Output :/n  $(<$1.exp)"
-echo "Recived Output :/n $(<$1.out)"
+printf "Running Test Case $1 \n\n"
+printf "Looking For Expected Output :\n  $(<$1.exp)"
+printf "Recived Output :\n $(<$1.out)"
 
 # Commented Out until Later Stages
 #diff -q $1.exp $1.out
