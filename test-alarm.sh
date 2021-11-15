a50e723490fce16dabfbe26732ae6acd75e3019c
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 14:39:58 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -8,14 +8,12 @@ do
 done
 
 
-./a.out "5 chains" "4 cattle" "2 milk" >> $1.out
-
+./a.out "5 chains" "4 cattle" "2 milk" >> $1.out &
 
 sleep 40
-#p=`echo $!`
  # Max time we will wait for a given test
 #get the process Id of teh
-#kill -15 $p # 15 is the code for SIGTERM or CNTL +C inside of a Program. 
+kill -15 $! # 15 is the code for SIGTERM or CNTL +C inside of a Program. 
 
 
 printf "\n\nRunning Test Case $1 \n Input is :\n  $(<$1.test) \n\n" 
