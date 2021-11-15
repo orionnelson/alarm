dd97d7f5e95aecdfaa69c9db627f8fa6c16fc896
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 13:41:34 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 input=`cat $1.test`
-./a.out $input >> $1.out 2>&1 
+./a.out `$input` >> $1.out 2>&1 
 #p=`echo $!`
 sleep 40 # Max time we will wait for a given test
 #get the process Id of teh
