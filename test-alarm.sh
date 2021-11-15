d08e1bc5acd54feb97ed6e01e92f49c89df085d8
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 17:14:32 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -9,9 +9,7 @@ done
 
 
 timeout 30s ./My_Alarm "5 chains" "4 cattle" "2 milk" >> $1.out
-wait $!
-
-sleep 30
+echo `cat $1.out`
 
  # Max time we will wait for a given test
 #get the process Id of teh
