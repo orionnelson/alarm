7edc7d909a65c29bc6628782f74692a862c9bf88
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 13:26:18 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -3,11 +3,12 @@ set -e
 input=`cat $1.test`
 ./a.out $input >> $1.out 2>&1 
 #pid=`pidof a.out`
+p=$(pidof a.out)
 sleep 100 # Max time we will wait for a given test
 #get the process Id of teh
 wait -n
 wait -n 
-kill -15 $pid # 15 is the code for SIGTERM or CNTL +C inside of a Program. 
+kill -15 $p # 15 is the code for SIGTERM or CNTL +C inside of a Program. 
 
 
 printf "\n\nRunning Test Case $1 \n Input is :\n  $(<$1.test) \n\n" 
