2760d442bd4a4f7c5d22d7befbda8439167f50fe
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 15:21:02 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -10,28 +10,15 @@ done
 
 ./My_Alarm "5 chains" "4 cattle" "2 milk" >> $1.out &
 
-
-printf "$n_procs"
-
-for i in $n_procs; do
-    ./procs[${i}] &
-    pids[${i}]=$!
-done
-
-# wait for all pids
-for pid in ${pids[*]}; do
-    wait $pid
-done
-
+sleep 40
 
  # Max time we will wait for a given test
 #get the process Id of teh
 #kill -15 $! # 15 is the code for SIGTERM or CNTL +C inside of a Program. 
-
-
+output=`cat $1.out`
 printf "\n\nRunning Test Case $1 \n Input is :\n  $(<$1.test) \n\n" 
 printf "\nLooking For Expected Output :\n  $(<$1.exp)\n"
-printf "\nRecived Output :\n $(<$1.out)\n"
+printf "\nRecived Output :\n $output\n"
 
 # Commented Out until Later Stages
 #diff -q $1.exp $1.out
