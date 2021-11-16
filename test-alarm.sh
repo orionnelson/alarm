cd45579cc958ff9f28615e066c875de9cd3c50ab
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 21:11:17 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+PATH=$PATH:/usr/bin/
 set -e
+
+#"${inputs[@]}"
 
 printf "HERE IS TEST ALARM"
 readarray inputs < $1.test
 rm -f $1.out
-#timeout -s SIGINT 30s ./My_Alarm "${inputs[@]}" >> $1.out # Kills current shell fml
-timeout -s SIGINT 30s ./My_Alarm "${inputs[@]}" >> $1.out
+printf "$1.out"
+sleep 1
+unbuffer timeout -s SIGINT 30s ./My_Alarm "${inputs[@]}" >> $1.out
 
-# I HATE SHELL
+# I HATE SHELL this line shoud work but it doesnt 
