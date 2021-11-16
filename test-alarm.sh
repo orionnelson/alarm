50f16b40c421039e3c2d08a54c8de9fc60deae2a
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 20:06:57 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
+
+printf "HERE IS TEST ALARM"
 readarray inputs < $1.test
 rm -f $1.out
 #timeout -s SIGINT 30s ./My_Alarm "${inputs[@]}" >> $1.out # Kills current shell fml
-printf "HERE IS TEST ALARM"
 timeout -s SIGINT 30s ./My_Alarm "${inputs[@]}" >> $1.out
 
 # I HATE SHELL
