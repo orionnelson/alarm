5f7b2beb8406e9cbf5b9986a199f743ff8337ceb
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 20:01:00 -0500

    Update timeout.sh

--- timeout.sh
+++ timeout.sh
@@ -3,6 +3,9 @@ set -e
 echo "HERE IS TIMEOUT"
 sleep 35
 
+
+
+echo `cat $1.out`
 printf "\n\nRunning Test Case $1 \n Input is :\n  $(<$1.test) \n\n" 
 printf "\nLooking For Expected Output :\n  $(<$1.exp)\n"
 printf "\nRecived Output :\n $(<$1.out)\n"
