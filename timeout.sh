a9c5eac365d627ae608d789bf22c345a64f1363d
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 19:28:44 -0500

    Add files via upload

---
+++ timeout.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+echo $1
+sleep 35
+
+printf "\n\nRunning Test Case $1 \n Input is :\n  $(<$1.test) \n\n" 
+printf "\nLooking For Expected Output :\n  $(<$1.exp)\n"
+printf "\nRecived Output :\n $(<$1.out)\n"
+
+# Commented Out until Later Stages
+#diff -q $1.exp $1.out
