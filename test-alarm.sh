3988393d3f2559e3f74a2e1a987461a73bc80f39
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 22:07:48 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -9,6 +9,6 @@ readarray inputs < $1.test
 rm -f $1.out
 printf "$1.out is the test being run"
 sleep 1
-unbuffer timeout -s SIGINT 120s ./My_Alarm "${inputs[@]}" >> $1.out
+unbuffer timeout -s SIGINT 33s ./My_Alarm "${inputs[@]}" >> $1.out
 
 # I HATE SHELL this line shoud work but it doesnt 
