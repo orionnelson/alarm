b5a8c0a317c64ed36e73c3101749bd8e5180ef84
Orion Nelson <44347765+orionnelson@users.noreply.github.com>
2021-11-15 22:08:03 -0500

    Update test-alarm.sh

--- test-alarm.sh
+++ test-alarm.sh
@@ -7,7 +7,7 @@ set -e
 #printf "HERE IS TEST ALARM"
 readarray inputs < $1.test
 rm -f $1.out
-printf "$1.out is the test being run"
+#printf "$1.out is the test being run"
 sleep 1
 unbuffer timeout -s SIGINT 33s ./My_Alarm "${inputs[@]}" >> $1.out
 
